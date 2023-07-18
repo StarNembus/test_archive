5e1c0967766810af0ad76976e7d6eef3d95f8c5b
StarNembus <larolimp@gmail.com>
2023-07-18 11:18:19 +0300

    Remove constructor

--- src/main/java/com/example/test/MainView.java
+++ src/main/java/com/example/test/MainView.java
@@ -3,11 +3,9 @@ package com.example.test;
 import com.example.test.entity.MyCounterEntity;
 import com.example.test.repositories.MyRepository;
 import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.html.NativeLabel;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.IntegerField;
 import com.vaadin.flow.router.Route;
-import org.springframework.beans.factory.annotation.Autowired;
 
 
 /**
@@ -18,14 +16,11 @@ import org.springframework.beans.factory.annotation.Autowired;
  */
 @Route("main")
 public class MainView extends VerticalLayout {
-    @Autowired
+
     private MyRepository myRepository;
 
     public MainView(MyRepository myRepository) {
         this.myRepository = myRepository;
-    }
-
-    public MainView() {
         IntegerField countField = new IntegerField();
         MyCounterEntity myCounterEntity = new MyCounterEntity(0);
         countField.setValue(myCounterEntity.getMyCounter());
@@ -35,14 +30,14 @@ public class MainView extends VerticalLayout {
         button.addClickListener(clickEvent -> {
             myCounterEntity.setMyCounter(myCounterEntity.getMyCounter() + 1);
             countField.setValue(myCounterEntity.getMyCounter());
-            myRepository.save(myCounterEntity);
+            this.myRepository.save(myCounterEntity);
         });
         add(button);
 
         Button buttonSave = new Button("Сохранить");
         buttonSave.addClickListener(clickEvent -> {
             myCounterEntity.setMyCounter(countField.getValue());
-            myRepository.save(myCounterEntity);
+            this.myRepository.save(myCounterEntity);
         });
         add(buttonSave);
 
